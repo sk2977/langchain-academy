9c7402bcbad9ea1fac7ee88174b62ced91e0ba5f
sk2977 <32197446+sk2977@users.noreply.github.com>
2024-11-24 18:34:07 -0500

    update

--- lc-academy-env/Scripts/activate.bat
+++ lc-academy-env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Spencer Work\Documents\GitHub\LangGraph\langchain-academy\lc-academy-env
+set VIRTUAL_ENV=C:\Users\Spencer Work\Documents\GitHub\langchain-academy\lc-academy-env
 
 if not defined PROMPT set PROMPT=$P$G
 
